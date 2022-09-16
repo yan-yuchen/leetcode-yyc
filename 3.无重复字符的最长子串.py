40c19bfaccc5fcf057f4e286c812f428e59d9d80
chenchen <chenchen@LAPTOP-UP9HINRN.localdomain>
2022-09-16 09:48:40 +0800

    2022 9 16

---
+++ 3.无重复字符的最长子串.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode.cn id=3 lang=python3
+#
+# [3] 鏃犻噸澶嶅瓧绗︾殑鏈�闀垮瓙涓?
+#
+
+# @lc code=start
+
+# 滑动窗口法
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        w = []  # 滑动窗口
+        max_length = 0
+
+        # 遍历字符串
+        for c in s:
+            # 如果字符不在滑动窗口中，将此字符串加入窗口
+            if c not in w:
+                w.append(c)
+
+            # 如果字符串在窗口中已经存在
+            else:
+                # 从窗口中移除重复字符及之前的字符串部分，新字符串即为无重复字符的字符串
+                w[:] = w[w.index(c)+1:]
+                # 将当前字符加入窗口
+                w.append(c)
+
+            max_length = max(len(w), max_length)
+
+        return max_length
+
+
+# @lc code=end
---
+++ 4.寻找两个正序数组的中位数.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=4 lang=python3
+#
+# [4] 寻找两个正序数组的中位数
+#
+
+# @lc code=start
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        temp = sorted(nums1+nums2)
+
+        if (len(temp) % 2 != 0):
+            return temp[len(temp)//2]
+        else:
+            return (temp[len(temp)//2-1]+temp[len(temp)//2])/2
+
+# @lc code=end
